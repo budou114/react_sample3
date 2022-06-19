1743faf9b638ccac846c02ca5932eb1d72dc8a25
budou114 <58349030+budou114@users.noreply.github.com>
2022-06-19 12:20:39 +0900

    404ページを用意する

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -2,6 +2,7 @@ import { Switch, Route } from "react-router-dom";
 import { Home } from "../Home";
 import { page1Routes } from "./Page1Routes";
 import { page2Routes } from "./Page2Routes";
+import { Page404 } from "../Page404";
 
 export const Router = () => {
   return (
@@ -29,6 +30,9 @@ export const Router = () => {
           ))}
         </Switch>
       )}>
+      </Route>
+      <Route path="*">
+        <Page404 />
       </Route>
     </Switch>
   );
